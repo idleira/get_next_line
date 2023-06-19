2695081ae1e7c534400d9e4718c753eb42cb6491
ibeliaie <ibeliaie@student.42wolfsburg.de>
2023-06-19 12:42:51 +0200

    working version

---
+++ get_next_line.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ibeliaie <ibeliaie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 18:43:50 by ibeliaie          #+#    #+#             */
+/*   Updated: 2023/06/19 12:35:37 by ibeliaie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_extract_line(char *rest)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!rest[i])
+		return (NULL);
+	while (rest[i] && rest[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (rest[i] && rest[i] != '\n')
+	{
+		str[i] = rest[i];
+		i++;
+	}
+	if (rest[i] == '\n')
+	{
+		str[i] = rest[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_read_till_nl(int fd, char *rest)
+{
+	char	*buff;
+	int		bytes_read;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(rest, '\n') && bytes_read != 0)
+	{
+	bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+	buff[bytes_read] = '\0';
+	rest = ft_strjoin(rest, buff);
+	}
+	free(buff);
+	return (rest);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*rest;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	rest = ft_read_till_nl(fd, rest);
+	if (!rest)
+		return (NULL);
+	line = ft_extract_line(rest);
+	rest = ft_new_rest(rest);
+	return (line);
+}
